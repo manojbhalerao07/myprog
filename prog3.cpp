9e204b92d4e251871859672a93e3f878a8c241a0
manojbhalerao07 <94183644+manojbhalerao07@users.noreply.github.com>
2021-11-15 13:32:59 +0530

    Add files via upload

---
+++ prog3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int input_1 = 50;
+cout <<input_1;
+return 0;
+}
+
+
---
+++ prog4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int input_1= 50, input_2= 60;
+cout <<value of input_1 is= "\n";
+cout << value of input_2 is=;
+return 0;
+}
